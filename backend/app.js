ccfdca2ccb3e5a2479d3f4d9226770557577fd93
Vijayalakshmi V <vijayalakshmi.v@HCL.COM>
2019-05-06 16:44:51 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const postroutes = require("./routes/posts");
+
+const Post = require("./models/post");
+
+const app = express();
+
+const MongoClient = require("mongodb").MongoClient;
+
+mongoose.set('debug', true);
+
+mongoose
+  .connect(
+    "mongodb://localhost:27017/local",{ useNewUrlParser: true } 
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(err => {
+    console.error('App starting error:', err.stack);
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images",express.static(path.join("backend/images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST,PUT, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts",postroutes);
+module.exports = app;
